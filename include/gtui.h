a3daefe4dc969dd763e2011469b98dd3fa3842de
Gabro <gabriele.cocchieri@gmail.com>
2026-03-16 12:59:20 +0100

    Update gtui.h

--- include/gtui.h
+++ include/gtui.h
@@ -11,13 +11,63 @@
 #endif
 
 /* == ESCAPE SEQUENCES (use with printf == */
-#define GTUI_ESC_CLEAR "\033[J"
 #define GTUI_ESC_UP "\033[1A"
 #define GTUI_ESC_DOWN "\033[1B"
 #define GTUI_ESC_RIGHT "\033[1C"
 #define GTUI_ESC_LEFT "\033[1D"
 #define GTUI_ESC_DOWN_BEGIN "\033[1E" //Down and to the beginning of the line
 #define GTUI_ESC_UP_BEGIN "\033[1F" //Up and to the beginning of the line
+
+#define GTUI_ESC_ERASE_CURSOR_TO_END "\033[0J"
+#define GTUI_ESC_ERASE_CURSOR_TO_BEGIN "\033[1J"
+#define GTUI_ESC_ERASE_ALL "\033[2J"
+#define GTUI_ESC_ERASE_CURSOR_TO_LINE_END "\033[0K"
+#define GTUI_ESC_ERASE_CURSOR_TO_LINE_START "\033[1K"
+#define GTUI_ESC_ERASE_LINE "\033[2K"
+
+#define GTUI_ESC_RESET_STYLE "\033[0m"
+#define GTUI_ESC_ENABLE_BOLD "\033[1m"
+#define GTUI_ESC_DISABLE_BOLD "\033[22m"
+#define GTUI_ESC_ENABLE_ITALIC "\033[3m"
+#define GTUI_ESC_DISABLE_ITALIC "\033[23m"
+#define GTUI_ESC_ENABLE_UNDERLINE "\033[4m"
+#define GTUI_ESC_DISABLE_UNDERLINE "\033[24m"
+
+#define GTUI_ESC_FG_BLACK "\033[30m"
+#define GTUI_ESC_BG_BLACK "\033[40m"
+#define GTUI_ESC_FG_RED "\033[31m"
+#define GTUI_ESC_BG_RED "\033[41m"
+#define GTUI_ESC_FG_GREEN "\033[32m"
+#define GTUI_ESC_BG_GREEN "\033[42m"
+#define GTUI_ESC_FG_YELLOW "\033[33m"
+#define GTUI_ESC_BG_YELLOW "\033[43m"
+#define GTUI_ESC_FG_BLUE "\033[34m"
+#define GTUI_ESC_BG_BLUE "\033[44m"
+#define GTUI_ESC_FG_MAGENTA "\033[35m"
+#define GTUI_ESC_BG_MAGENTA "\033[45m"
+#define GTUI_ESC_FG_CYAN "\033[36m"
+#define GTUI_ESC_BG_CYAN "\033[46m"
+#define GTUI_ESC_FG_WHITE "\033[37m"
+#define GTUI_ESC_BG_WHITE "\033[47m"
+#define GTUI_ESC_FG_DEFAULT "\033[39m"
+#define GTUI_ESC_BG_DEFAULT "\033[49m"
+
+#define GTUI_ESC_FG_BRIGHT_BLACK "\033[90m"
+#define GTUI_ESC_BG_BRIGHT_BLACK "\033[100m"
+#define GTUI_ESC_FG_BRIGHT_RED "\033[91m"
+#define GTUI_ESC_BG_BRIGHT_RED "\033[101m"
+#define GTUI_ESC_FG_BRIGHT_GREEN "\033[92m"
+#define GTUI_ESC_BG_BRIGHT_GREEN "\033[102m"
+#define GTUI_ESC_FG_BRIGHT_YELLOW "\033[93m"
+#define GTUI_ESC_BG_BRIGHT_YELLOW "\033[103m"
+#define GTUI_ESC_FG_BRIGHT_BLUE "\033[94m"
+#define GTUI_ESC_BG_BRIGHT_BLUE "\033[104m"
+#define GTUI_ESC_FG_BRIGHT_MAGENTA "\033[95m"
+#define GTUI_ESC_BG_BRIGHT_MAGENTA "\033[105m"
+#define GTUI_ESC_FG_BRIGHT_CYAN "\033[96m"
+#define GTUI_ESC_BG_BRIGHT_CYAN "\033[106m"
+#define GTUI_ESC_FG_BRIGHT_WHITE "\033[97m"
+#define GTUI_ESC_BG_BRIGHT_WHITE "\033[107m"
 
 /* == API == */
 void gtuiInitialize();
@@ -72,7 +122,7 @@ void gtuiMoveCursor(uint8_t x, uint8_t y)
 
    	char str[12];
    	snprintf(str, sizeof(str), "\033[%d;%dH", y, x);
-   	
+
    	printf("%s", str);
 }
 
@@ -85,4 +135,4 @@ void gtuiCleanCharacters(uint8_t x, uint8_t y, uint8_t count, char resetCursorPo
 }
 
 #endif
-	
+
